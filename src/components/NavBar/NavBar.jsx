b66d06d185e06d57f048d0ad2ebdc78d53409946
adolfo nin <ionfiz92@gmail.com>
2022-10-16 18:43:33 -0300

    navbarv2

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,64 +1,30 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
-import CartIcon from "../CartIcon/CartIcon";
-import { FirebaseContext } from '../../firebase';
 
 const NavBar = () => {
-
-    const { user, firebase } = useContext( FirebaseContext );
-
-    return(
-        <ul id="button">
-            {user ? (
-                <li style={{ color: "white", fontWeight: "700"}}>
-                    Hola {user.displayName}
-                </li>
-            ) : (
-                <li></li>
-            )}
-            <li>
-                <Link to="/">
-                    Inicio
-                </Link>
-            </li>
-            <li>
-                <Link to="/">
-                    Productos
-                </Link>
-            </li>
-            { user ? (
-                <li style={{ color: "white" }} onClick={ () => firebase.logoutUser() }>
-                    <Link to="/">
-                        Cerrar sesión
-                    </Link>
-                </li>
-            ) : (
-                <>
-                    <li>
-                        <Link to="/register">
-                            Crear Cuenta
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/login">
-                            Iniciar sesión
-                        </Link>
-                    </li>
-                </>
-            )}
-            <li>
-                <Link to="/contact">
-                    Contacto
-                </Link>
-            </li>
-            <li>
-                <Link to="/cart">
-                    <CartIcon />
-                </Link>
-            </li>
-        </ul>
-    );
-}
+  return (
+    <>
+      <header>
+        <nav className='navbar'>
+          <Link to='/' className='navbar__title'>
+            Nexus Bro Rental
+          </Link>
+          <ul className='navbar__list'>
+            <Link to='/' className='navbar__list-item'>
+              Home
+            </Link>
+            <Link to='/catalogo' className='navbar__list-item'>
+              Catalogo
+            </Link>
+            <Link to='/carrito' className='navbar__list-item'>
+              Carrito
+            </Link>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
 
 export default NavBar;
